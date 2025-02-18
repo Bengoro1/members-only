a665e3fc04e48f93368763f9bc0d41cf8a2f0874
Bengoro <127534872+Bengoro1@users.noreply.github.com>
2025-02-18 16:55:32 +0100

    validate member-form

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,37 @@
 require('dotenv').config();
 const db = require('../db/userQueries');
+const {body, validationResult} = require('express-validator');
+
+const validatePasscode = [
+  body('passcode')
+    .custom(value => {
+      if (value.toLowerCase() != process.env.MEMBERSHIP_PASSWORD.toLowerCase()) {
+        throw new Error('Wrong passcode, try again!');
+      }
+      return true;
+    })
+];
 
 const getMemberForm = (req, res) => {
   res.render('member-form');
 }
 
-const memberPasscode = async (req, res) => {
-  const {passcode} = req.body;
-  if (process.env.MEMBERSHIP_PASSWORD == passcode) {
-    await db.grantMembership(req.user.id);
-    res.redirect('/');
-  } else {
-    res.render('member-form', {allert: 'Wrong passcode, try again!'});
+const memberPasscode = [
+  validatePasscode, async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).render('member-form', {
+        errors: errors.array(),
+      });
+    }
+    try {
+      await db.grantMembership(req.user.id);
+      res.redirect('/');
+    } catch (err) {
+      next(err);
+    }
   }
-}
+];
 
 module.exports = {
   getMemberForm,
